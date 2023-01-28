55bd13dd053601d16e8b77eca8695d56cfb3e35e
L2____a <121000892+L2-a@users.noreply.github.com>
2023-01-28 22:29:12 +0900

    login-js

---
+++ js/background.js
@@ -0,0 +1,16 @@
+const images = [
+  "../photo/ben-black.jpg",
+  "../photo/ben.jpg",
+  "../photo/london-black.jpg",
+  "../photo/london.jpg",
+  "../photo/london.jpg",
+  "../photo/pink-sky.jpg",
+  "../photo/star-tree.jpg",
+];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+const bgImage = document.createElement("img");
+const background = document.querySelector(".image");
+
+bgImage.src = `img/${chosenImage}`;
+background.prepend(bgImage);
